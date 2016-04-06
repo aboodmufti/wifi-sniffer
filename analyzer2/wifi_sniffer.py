642d3b9e55a70bc605160de8a29a1176c9532567
Abood Mufti <mufti14@gmail.com>
2016-04-06 18:16:04 -0400

    added MAC addresses parsing

--- analyzer2/wifi_sniffer.py
+++ analyzer2/wifi_sniffer.py
@@ -26,9 +26,8 @@ ap_list = set()
 ssid_list = set()
 while True :
   pkt = rawSocket.recv(4096)
-  #print "GOT HERE"
+  
   if pkt[0] == "\x00" : 
-    #print "ZERO: pad: %r | length: %r %r" %(pkt[1],pkt[2:4],pkt[4:8])
     radio_version = unpack('<B',pkt[0])[0]
     radio_pad = unpack('<B',pkt[1])[0]
     radio_length = unpack('<H',pkt[2:4])[0]
@@ -37,70 +36,54 @@ while True :
     #print("%s" %bin(int(unpack('<I',pkt[4:8])[0])))
     binNum = bin(int(unpack('<I',pkt[4:8])[0]))
     reveresedBin = reverseString(removefirst2Chars(binNum))
-    #print "present flags: "
-    present = ""
     offset = 0
     json = "{"
     for i in range(len(reveresedBin)):
 		if reveresedBin[i] == "1":
 			if i == 5:
-				present += flags[i]
-				present += ": %s \n" %unpack('<b',pkt[14:15])[0]
 				json += "\"SINGAL\" : %s ," %(unpack('<b',pkt[14:15])[0])
 			if i == 3:
-				present += flags[i]
 				raw_channel = unpack('<H',pkt[10:12])[0]
-				present += ": %sMHZ - " %raw_channel
 				json += "\"FREQUENCY\" : %s ," %raw_channel
 				found = False
 				for i in range(len(channels)):
 					if raw_channel == channels[i]:
-						present += "%i \n" %i
 						json += "\"CHANNEL\" : %s ," %i
 						found = True
 						break
 				if found == False:
 					for i in range(len(frequencies_5)):
 						if raw_channel == frequencies_5[i]:
-							present += "%i \n" %channels_5[i]
 							json += "\"CHANNEL\" : %s ," %channels_5[i]
 							found = True
 							break
 			offset += flags_sizes[i]
 			
     nextByte = removefirst2Chars(bin(int(unpack('<B',pkt[radio_length])[0])))
-    #present += "next byte: " + nextByte
+    
     if nextByte[0:4] == "1000" and nextByte[4:6] == "00" and nextByte[6:8] == "00":
-		#present += "*******************************Beacon Frame \n"
-		addr = pkt[radio_length+10:radio_length+16]
-		addr1 = pkt[radio_length+10]
-		addr2 = pkt[radio_length+11]
-		addr3 = pkt[radio_length+12]
-		addr4 = pkt[radio_length+13]
-		addr5 = pkt[radio_length+14]
-		addr6 = pkt[radio_length+15]
 		
-		#present += "MAC ADDRESS: %r - %r - %r - %r - %r - %r \n" %(addr1,addr2,addr3,addr4,addr5,addr6)
-		#json += "MAC_ADDRESS: '%r - %r - %r - %r - %r - %r' ," %(addr1,addr2,addr3,addr4,addr5,addr6)
+		addr1 = hex(unpack('<B',pkt[radio_length+10])[0])[2:]
+		addr2 = hex(unpack('<B',pkt[radio_length+11])[0])[2:]
+		addr3 = hex(unpack('<B',pkt[radio_length+12])[0])[2:]
+		addr4 = hex(unpack('<B',pkt[radio_length+13])[0])[2:]
+		addr5 = hex(unpack('<B',pkt[radio_length+14])[0])[2:]
+		addr6 = hex(unpack('<B',pkt[radio_length+15])[0])[2:]
+		addr = "%s:%s:%s:%s:%s:%s" %(addr1,addr2,addr3,addr4,addr5,addr6)
+		json += "MAC_ADDRESS: '%s:%s:%s:%s:%s:%s' ," %(addr1,addr2,addr3,addr4,addr5,addr6)
+		
 		beaconFrame = pkt[radio_length+36:]
 		
 		id = int(unpack('<B',beaconFrame[0])[0])
 		if id == 0:
 			length = int(unpack('<B',beaconFrame[1])[0])
-			present += "SSID: "
 			ssid_name = ""
 			for j in range(0,length):
 				char = unpack('<c',beaconFrame[2+j])[0]
-				present += "%s" %char
 				ssid_name += "%s" %char
-			present += "\n"
 			json += "\"SSID\" : \"%s\" }" %ssid_name
-			print json 
-			"""if addr in ap_list:
-				present = ""
-			else:
-				#present += "\n"
+			if addr not in ap_list:
 				ssid_list.add(ssid_name)
 				ap_list.add(addr)
 				print json 
-            """
+            
