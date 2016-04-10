8ef3cefadcecedcba422ebee0c107792c3f578ed
Abood Mufti <mufti14@gmail.com>
2016-04-10 13:20:56 -0400

    unexpected token avoided

--- analyzer2/app.js
+++ analyzer2/app.js
@@ -23,11 +23,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 var PythonShell = require("python-shell")
 var pyshell = new PythonShell('wifi_sniffer.py');
 var serverData = []
+var message = ""
 pyshell.on('message', function (message) {
     // received a message sent from the Python script (a simple "print" statement) 
-    var obj = JSON.parse(message)
-    //console.log(obj)
-    serverData.push(obj)
+
+    if(message.substr(0,2) == "{\""  && message.substr(-3) == "\" }"){
+      try{
+        var obj = JSON.parse(message)
+        serverData.push(obj)
+      }catch(e){
+        console.log(e.message);
+      }
+    }
+    
     
 });
 
@@ -36,36 +44,36 @@ app.get("/data", function(req, res) {
 
   
   var finalData = {
-        labels: [0,1,2,3,4,5,6,7,8,9,10,11],
+        labels: [" "," ",1,2,3,4,5,6,7,8,9,10,11," "," "],
         datasets: []
     };
     
-  //console.log(finalData)
   var unique_AP = []
-  //console.log("DEBUG 2")
   function inUnique(BSSID){
-      //console.log(unique_AP)
       
       for(var i = 0; i<unique_AP.length; ++i){
-          if(unique_AP[i] && BSSID == unique_AP[i].SSID){
+          /*if(unique_AP[i] && BSSID == unique_AP[i].SSID){
+              return [true,i]
+          }*/
+          if(unique_AP[i] && BSSID == unique_AP[i].MAC_ADDRESS){
               return [true,i]
           }
       }
       return [false,null] 
   }
   
-  //console.log("DEBUG 3")
   for(var i = 0; i<serverData.length; ++i){
       currData = serverData[i]
-      exists = inUnique(currData.SSID)
+      //exists = inUnique(currData.SSID)
+      exists = inUnique(currData.MAC_ADDRESS)
       oldID = exists[1]
       if(currData.CHANNEL > 11){
           continue
       }
       if(exists[0]){
-          if(currData.SIGNAL < unique_AP[oldID]){
+          //if(currData.SIGNAL < unique_AP[oldID]){
               unique_AP[oldID].SIGNAL = currData.SIGNAL
-          }
+          //}
       }else{
           //if(currData != undefined){
             unique_AP.push(currData)
@@ -73,43 +81,47 @@ app.get("/data", function(req, res) {
       }   
   }
   
-  //console.log("DEBUG 4")
   for(var i = 0; i<unique_AP.length; ++i){
       var values = []
       if(unique_AP[i] == undefined){
         continue  
       }
       var channel = unique_AP[i].CHANNEL
-      //console.log("channel: "+channel)
-      for(var j = 0 ; j<11; ++j){
-        
-          if(j == channel - 1){
-              values.push(-90)
+      channel += 1
+      for(var j = 0 ; j<15; ++j){
+          var currSignal = unique_AP[i].SIGNAL
+          
+          if(j == channel - 2){
+              values.push(-100)
+          }else if(j == channel - 1){
+              values.push(null)
           }else if(j ==  channel){
-              //console.log(unique_AP[i])
-              values.push(unique_AP[i].SIGNAL)
+              values.push(currSignal)
           }else if(j ==  channel +1){
-              values.push(-90)
+              values.push(null)
+          }else if(j ==  channel +2){
+              values.push(-100)
           }else{
               values.push(null)
           }
       }
-      
+      /*
+        highlightFill: "rgba("+r+","+g+","+b+",0.75)",
+        highlightStroke: "rgba("+r+","+g+","+b+",1)",
+       * */
       var r = Math.floor((Math.random() * 220) + 20).toString();
       var g = Math.floor((Math.random() * 220) + 20).toString();
       var b = Math.floor((Math.random() * 220) + 20).toString();
       var dataset = {
-          label: unique_AP[i].SSID ,
-          fillColor: "rgba("+r+","+g+","+b+",0.5)",
-          strokeColor: "rgba("+r+","+g+","+b+",0.8)",
-          highlightFill: "rgba("+r+","+g+","+b+",0.75)",
-          highlightStroke: "rgba("+r+","+g+","+b+",1)",
+          label: unique_AP[i].SSID +" - "+unique_AP[i].MAC_ADDRESS.substring(0,5) ,
+          fillColor: "rgba("+r+","+g+","+b+",0.3)",
+          strokeColor: "rgba("+r+","+g+","+b+",0.9)",
           data: values
       }
       finalData.datasets.push(dataset)
   }
-  console.log(unique_AP)
-  res.json(finalData)
+  //console.log(finalData.datasets)
+  res.send(finalData)
   
   finalData = []
 
--- analyzer2/wifi_sniffer.py
+++ analyzer2/wifi_sniffer.py
@@ -11,13 +11,13 @@ frequencies_5 = [5180,5190,5200,5210,5220,5230,5240,5745,5755,5765,5775,5785,579
 channels_5 = [36,38,40,42,44,46,48,149,151,153,155,157,159,161,165]
 
 def reverseString(str):
-	return str[::-1]
+  return str[::-1]
 
 def removefirst2Chars(str):
-	return str[2:]
-	
+  return str[2:]
+  
 def getCharAt(str,i):
-	return strp[i:i+1]
+  return strp[i:i+1]
 
 #sudo airmon-ng start wlan0
 rawSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
@@ -39,56 +39,63 @@ while True :
     offset = 0
     json = "{"
     for i in range(len(reveresedBin)):
-		if reveresedBin[i] == "1":
-			if i == 5:
-				json += "\"SIGNAL\" : %s ," %(unpack('<b',pkt[14:15])[0])
-			if i == 3:
-				raw_channel = unpack('<H',pkt[10:12])[0]
-				json += "\"FREQUENCY\" : %s ," %raw_channel
-				found = False
-				for i in range(len(channels)):
-					if raw_channel == channels[i]:
-						json += "\"CHANNEL\" : %s ," %i
-						found = True
-						break
-				if found == False:
-					for i in range(len(frequencies_5)):
-						if raw_channel == frequencies_5[i]:
-							json += "\"CHANNEL\" : %s ," %channels_5[i]
-							found = True
-							break
-			offset += flags_sizes[i]
-			
+      if reveresedBin[i] == "1":
+        if i == 5:
+          json += "\"SIGNAL\" : %s ," %(unpack('<b',pkt[14:15])[0])
+        if i == 3:
+          raw_channel = unpack('<H',pkt[10:12])[0]
+          json += "\"FREQUENCY\" : %s ," %raw_channel
+          found = False
+          for i in range(len(channels)):
+            if raw_channel == channels[i]:
+              json += "\"CHANNEL\" : %s ," %i
+              found = True
+              break
+          if found == False:
+            for i in range(len(frequencies_5)):
+              if raw_channel == frequencies_5[i]:
+                json += "\"CHANNEL\" : %s ," %channels_5[i]
+                found = True
+                break
+        offset += flags_sizes[i]
+      
     nextByte = removefirst2Chars(bin(int(unpack('<B',pkt[radio_length])[0])))
     
     if nextByte[0:4] == "1000" and nextByte[4:6] == "00" and nextByte[6:8] == "00":
-		foundIt = False
-		addr1 = hex(unpack('<B',pkt[radio_length+10])[0])[2:]
-		if addr1 == "4a":
-		 foundIt = True
-     # print "============================================================================================================================================================"
-		addr2 = hex(unpack('<B',pkt[radio_length+11])[0])[2:]
-		addr3 = hex(unpack('<B',pkt[radio_length+12])[0])[2:]
-		addr4 = hex(unpack('<B',pkt[radio_length+13])[0])[2:]
-		addr5 = hex(unpack('<B',pkt[radio_length+14])[0])[2:]
-		addr6 = hex(unpack('<B',pkt[radio_length+15])[0])[2:]
-		addr = "%s:%s:%s:%s:%s:%s" %(addr1,addr2,addr3,addr4,addr5,addr6)
-		json += "\"MAC_ADDRESS\": \"%s:%s:%s:%s:%s:%s\" ," %(addr1,addr2,addr3,addr4,addr5,addr6)
-		
-		beaconFrame = pkt[radio_length+36:]
-		
-		id = int(unpack('<B',beaconFrame[0])[0])
-		if id == 0:
-			length = int(unpack('<B',beaconFrame[1])[0])
-			ssid_name = ""
-			for j in range(0,length):
-				char = unpack('<c',beaconFrame[2+j])[0]
-				ssid_name += "%s" %char
-			json += "\"SSID\" : \"%s\" }" %ssid_name
-			#if addr not in ap_list:
-				#ssid_list.add(ssid_name)
-			#ap_list.add(addr)
-			if foundIt == False:
-			  print json 
-			
+      foundIt = False
+      addr1 = hex(unpack('<B',pkt[radio_length+10])[0])[2:]
+      if addr1 == "4a":
+        foundIt = True
+        #print "============================================================================================================================================================"
+      addr2 = hex(unpack('<B',pkt[radio_length+11])[0])[2:]
+      addr3 = hex(unpack('<B',pkt[radio_length+12])[0])[2:]
+      addr4 = hex(unpack('<B',pkt[radio_length+13])[0])[2:]
+      addr5 = hex(unpack('<B',pkt[radio_length+14])[0])[2:]
+      addr6 = hex(unpack('<B',pkt[radio_length+15])[0])[2:]
+      addr = "%s:%s:%s:%s:%s:%s" %(addr1,addr2,addr3,addr4,addr5,addr6)
+      json += "\"MAC_ADDRESS\": \"%s:%s:%s:%s:%s:%s\" ," %(addr1,addr2,addr3,addr4,addr5,addr6)
+      
+      beaconFrame = pkt[radio_length+36:]
+      
+      id = int(unpack('<B',beaconFrame[0])[0])
+      if id == 0:
+        length = int(unpack('<B',beaconFrame[1])[0])
+        ssid_name = ""
+        for j in range(0,length):
+          char = unpack('<c',beaconFrame[2+j])[0]
+          ssid_name += "%s" %char
+        json += "\"SSID\" : \"%s\" }" %ssid_name
+        
+        print json
+        #if addr not in ap_list:
+          #ssid_list.add(ssid_name)
+        #ap_list.add(addr)
+        #if foundIt == False:
+        #print json
+        """if(json[-10:] == "\"SSID\" : \""):
+          json += "\"}"
+          print "============================================================================================================================================================"
+          print "%s" %ssid_name 
+        """
+      
             
